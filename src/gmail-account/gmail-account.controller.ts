5aab8ce35ff8d651914e5aea9fc02f59bba62ad0
dixonmyles <dixon.myles@gmail.com>
2023-11-10 23:55:37 -0500

    Added filter for exception handling to controller

--- src/gmail-account/gmail-account.controller.ts
+++ src/gmail-account/gmail-account.controller.ts
@@ -6,13 +6,16 @@ import {
   Patch,
   Post,
   Query,
+  UseFilters,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { GmailAccountService } from './gmail-account.service';
 import { CreateGmailAccountDTO } from './dtos/create-gmail-account.dto';
 import { UpdateGmailAccountDTO } from './dtos/update-gmail-account.dto';
+import { HttpExceptionFilter } from '../shared/filters/http-exception.filter';
 
+@UseFilters(HttpExceptionFilter)
 @UsePipes(ValidationPipe)
 @Controller('gmail-accounts')
 export class GmailAccountController {
