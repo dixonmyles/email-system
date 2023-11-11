ea05d446141a6128deb3489bea6ae3b740ec4be3
dixonmyles <dixon.myles@gmail.com>
2023-11-10 22:43:22 -0500

    Added google api oauth2 client and handlers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GmailAccountModule } from './gmail-account/gmail-account.module';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { GmailAccountModule } from './gmail-account/gmail-account.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
       logging: true,
       synchronize: false,
       migrationsTableName: 'typeorm_migrations',
--- src/gmail-account/gmail-account.service.ts
+++ src/gmail-account/gmail-account.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { GmailAccount } from './entities/gmail-account.entity';
 import { CreateGmailAccountDTO } from './dtos/create-gmail-account.dto';
 import { UpdateGmailAccountDTO } from './dtos/update-gmail-account.dto';
+import getOAuth2Client from '../shared/utils/getOAuth2Client';
 
 @Injectable()
 export class GmailAccountService {
@@ -12,13 +13,109 @@ export class GmailAccountService {
     private gmailAccountRepository: Repository<GmailAccount>,
   ) {}
 
-  async create(dto: CreateGmailAccountDTO): Promise<GmailAccount> {
+  async create(dto: CreateGmailAccountDTO): Promise<string> {
     const gmailAccount = this.gmailAccountRepository.create(dto);
-    return await this.gmailAccountRepository.save(gmailAccount);
+    await this.gmailAccountRepository.save(gmailAccount);
+    // OAuth 2 call for getting user email access
+    const oAuth2Client = getOAuth2Client();
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: ['https://mail.google.com/'],
+      state: dto.email,
+    });
+    return authUrl;
   }
 
   async update(id: string, dto: UpdateGmailAccountDTO): Promise<GmailAccount> {
     await this.gmailAccountRepository.update(id, dto);
     return await this.gmailAccountRepository.findOne({ where: { id } });
+  }
+
+  async getWebhook(query: { code: string; state: string }) {
+    const oAuth2Client = getOAuth2Client();
+    const { tokens } = await oAuth2Client.getToken(query.code);
+
+    const gmailAccount = await this.gmailAccountRepository.findOne({
+      where: { email: query.state },
+    });
+
+    const updates = {
+      access_token: tokens.access_token,
+      refresh_token: tokens.refresh_token,
+      scope: tokens.scope,
+      expiry_date: tokens.expiry_date,
+      token_type: tokens.token_type,
+    };
+
+    await this.gmailAccountRepository.save({
+      ...gmailAccount, // the existing account
+      ...updates, // the new updates
+    });
+
+    return {
+      message: 'Gmail account updated successfully.',
+      email: gmailAccount.email,
+    };
+  }
+
+  public isTokenExpired(tokenExpiryDate: number): boolean {
+    return tokenExpiryDate <= Date.now();
+  }
+
+  public async refreshToken(token: any): Promise<any> {
+    const oAuth2Client = getOAuth2Client(token);
+    const refreshedTokenResponse = await oAuth2Client.refreshAccessToken();
+    return refreshedTokenResponse.credentials;
+  }
+
+  public async updateTokenInDB(id: string, updatedToken: any): Promise<void> {
+    const user = await this.gmailAccountRepository.findOne({ where: { id } });
+    if (user) {
+      await this.gmailAccountRepository.update(user.email, updatedToken);
+    }
+  }
+
+  // Function to get a token from the database
+  public async getTokenFromDB(id: string): Promise<{
+    access_token: string;
+    refresh_token: string;
+    scope: string;
+    expiry_date: number;
+    token_type: string;
+  } | null> {
+    const user = await this.gmailAccountRepository.findOne({ where: { id } });
+    if (user) {
+      return {
+        access_token: user.access_token,
+        refresh_token: user.refresh_token,
+        scope: user.scope,
+        expiry_date: user.expiry_date,
+        token_type: user.token_type,
+      };
+    }
+
+    return null;
+  }
+
+  // Function to validate the token and get an updated token if it is expired
+  public async validateToken(id: string): Promise<any> {
+    const token = await this.getTokenFromDB(id);
+    if (token) {
+      if (this.isTokenExpired(token.expiry_date)) {
+        const credentials = await this.refreshToken(token);
+
+        const updatedToken = {
+          access_token: credentials.access_token,
+          refresh_token: credentials.refresh_token || token.refresh_token,
+          expiry_date: credentials.expiry_date || token.expiry_date,
+          token_type: credentials.token_type || token.token_type,
+        };
+
+        await this.updateTokenInDB(id, updatedToken);
+        return updatedToken;
+      }
+      return token;
+    }
+    return null;
   }
 }
